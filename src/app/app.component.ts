ec30f4afc73eb769f44b1a7b34b3409558991ba7
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2016-12-22 18:11:25 +0100

    import Angular2TokenService
    Signed-off-by: Sébastien Gruhier <sebastien.gruhier@xilinus.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { Angular2TokenService } from 'angular2-token';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app works!';
